1684aa6d42caab2136d3336ba8fa94babf99548a
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-04-28 15:57:30 -0300

    comit troll

--- ServerChat/src/serverchat/ServerChat.java
+++ ServerChat/src/serverchat/ServerChat.java
@@ -14,6 +14,11 @@ public class ServerChat {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        //Teste TROLL
+        for (int t = 0; t < 100000000; t++) {
+
+            System.out.println(t);
+        }
     }
 }

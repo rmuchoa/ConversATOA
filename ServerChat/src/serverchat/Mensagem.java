8ee495e4ee00e4aff2509404e3c366872e6bf13d
Renan Marcel Uchôa <renanmarceluchoa@gmail.com>
2012-05-03 23:28:52 -0300

    classe mensagem criada: recebe um usuário, uma mensagem String, e flags que caracterizam se o status da mensagem é de conexão ou desconexão, e se o usuário já está autenticado no servidor como usuário logado.

---
+++ ServerChat/src/serverchat/Mensagem.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverchat;
+
+/**
+ *
+ * @author renanmarceluchoa
+ */
+public class Mensagem {
+    
+    private Usuario user;
+    private String message;
+    private boolean conStatus;
+    private boolean autentic;
+    
+    public Mensagem() {
+    
+    }
+
+    /**
+     * @return the user
+     */
+    public Usuario getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the conStatus
+     */
+    public boolean isConStatus() {
+        return conStatus;
+    }
+
+    /**
+     * @param conStatus the conStatus to set
+     */
+    public void setConStatus(boolean conStatus) {
+        this.conStatus = conStatus;
+    }
+
+    /**
+     * @return the autentic
+     */
+    public boolean isAutentic() {
+        return autentic;
+    }
+
+    /**
+     * @param autentic the autentic to set
+     */
+    public void setAutentic(boolean autentic) {
+        this.autentic = autentic;
+    }
+    
+}
